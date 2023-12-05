e1188bef79c4fc103d1ddf54ca86dbb9b98ebd00
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-05 18:53:03 +0530

    Added creator name

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  creator: 'Veerbal Singh',
   title:
     'QuickClinic: Streamlined Healthcare Appointments & Patient Management',
   description:
