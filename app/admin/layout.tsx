ffb18fa55d58147f17b2d6211a8815d0afde56c1
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-09 20:05:19 +0530

    Update admin layout with new navigation links

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,13 +2,24 @@ import Header from '../_components/layout/header';
 import Sidebar from './_components/sidebar';
 import SlideSidebar from './_components/slide-sidebar';
 
+const navLinks = [
+  {
+    title: 'Dashboard',
+    link: '/admin/dashboard',
+  },
+  {
+    title: 'Verify Doctors',
+    link: '/admin/verify-doctors',
+  },
+];
+
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full">
-      <Header menu={<SlideSidebar />} />
+      <Header menu={<SlideSidebar navLinks={navLinks} />} />
       <div className="pt-5 p-5 flex gap-2">
         <div className="hidden md:block">
-          <Sidebar />
+          <Sidebar navLinks={navLinks} />
         </div>
         <div className="flex-1">{children}</div>
       </div>
