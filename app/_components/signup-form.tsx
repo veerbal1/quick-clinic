0d210dc448eb3d2e5617e44079ebaca1537db31e
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-09 16:00:18 +0530

    Refactor signup form component to use zod for form validation

--- app/_components/signup-form.tsx
+++ app/_components/signup-form.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { HTMLAttributes, useState } from 'react';
 import { Icons } from './icons';
 import { cn } from '@/lib/utils';
@@ -10,48 +10,236 @@ import { signUpAction } from '@/lib/actions';
 import { useFormState, useFormStatus } from 'react-dom';
 import FormInput from './input';
 
+import * as z from 'zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { SelectContent } from '@radix-ui/react-select';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Label } from '@/components/ui/label';
+
+const formSchema = z
+  .object({
+    name: z
+      .string({ required_error: 'Name is required' })
+      .min(3, 'Name must be at least 3 characters long'),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    confirmPassword: z.string(),
+    gender: z
+      .string()
+      .refine(
+        (value) => value === 'male' || value === 'female',
+        "Gender must be either 'male' or 'female'"
+      ),
+    qualifications: z.string().optional(),
+    experience: z
+      .number({
+        required_error: 'Experience is required',
+      })
+      .int('Experience must be a whole number'),
+    bio: z.string(),
+    contactNumber: z
+      .string()
+      .refine(
+        (value) => value.toString().length === 10,
+        'Contact number must be 10 digits long'
+      ),
+    location: z.string().max(255),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
+
 interface UserAuthFormProps extends HTMLAttributes<HTMLDivElement> {}
 
 function SingupForm({ className, ...props }: UserAuthFormProps) {
   const [state, dispatch] = useFormState(signUpAction, undefined);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <div className={cn('grid gap-6', className)} {...props}>
-      <form action={dispatch}>
-        <div className="grid gap-2">
-          <div className="grid gap-2">
-            <FormInput
-              id="name"
-              label="Full Name"
-              autoComplete="name"
-              type="text"
-              required
-              placeholder="Full Name"
-              name="name"
-            />
-            <FormInput
-              id="email"
-              label="Email"
-              autoComplete="email"
-              type="email"
-              required
-              placeholder="Email"
-              name="email"
-            />
-            <FormInput
-              id="password"
-              label="Password"
-              autoComplete="password"
-              type="password"
-              required
-              placeholder="Password"
-              name="password"
-              min={6}
-            />
-            <p className="text-red-400 text-sm">{state}</p>
-            <SignUpButton />
-          </div>
-        </div>
-      </form>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="
+        grid gap-6 grid-cols-1 md:grid-cols-2
+        "
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Full Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="gender"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Gender</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select gender" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent className="bg-white w-full">
+                    <SelectItem value="male">Male</SelectItem>
+                    <SelectItem value="female">Female</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="qualifications"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Qualifications</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="experience"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Experience (Years)</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    min={0}
+                    max={100}
+                    {...field}
+                    onChange={(e) => field.onChange(e.target.valueAsNumber)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="bio"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Bio</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="contactNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contact Number</FormLabel>
+                <FormControl>
+                  <Input type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Location</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Sign Up</Button>
+        </form>
+      </Form>
     </div>
   );
 }
