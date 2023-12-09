b99a00eeb60f97dd97cebd9c5c8eee6acd3ea49c
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-09 20:04:54 +0530

    Add Table component

---
+++ app/_components/tables/index.tsx
@@ -0,0 +1,49 @@
+import {
+  TableCaption,
+  Table as ShadCnTable,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from '@/components/ui/table';
+
+function Table({
+  caption,
+  rows,
+  columns,
+}: {
+  caption: string;
+  rows: any[];
+  columns: {
+    className?: string;
+    header: string;
+    value: string | number | JSX.Element | JSX.Element[];
+  }[];
+}) {
+  return (
+    <ShadCnTable>
+      <TableCaption>{caption}</TableCaption>
+      <TableHeader>
+        <TableRow>
+          {columns.map((column) => (
+            <TableHead className={column.className}>{column.value}</TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell className={column.className}>
+                {row[column.header]}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </ShadCnTable>
+  );
+}
+
+export default Table;
