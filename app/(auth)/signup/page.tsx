680aa2430056217abbbdbf0755807b1a880ffd26
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-09 14:48:23 +0530

    Update signup page title and description

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -19,10 +19,10 @@ function SignUpPage() {
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
-              Create an account
+              Doctor&apos;s Signup
             </h1>
             <p className="text-sm text-muted-foreground">
-              Fill in the details below to start your journey with us
+              Fill in the details below to start your journey as a doctor
             </p>
           </div>
           <SingupForm />
