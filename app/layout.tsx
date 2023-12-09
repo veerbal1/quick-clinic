bb21088f5ebe35659ad6f1e66e9ad33ac402e93c
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-09 22:40:37 +0530

    Add Toaster component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="max-w-screen-2xl mx-auto">{children}</div>
+        <div className="max-w-screen-2xl mx-auto">
+          <Toaster />
+          {children}
+        </div>
       </body>
     </html>
   );
