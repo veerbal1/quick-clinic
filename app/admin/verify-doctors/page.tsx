165f25995e5a02e9736628d921c84be08f579325
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-09 21:27:50 +0530

    Refactor VerifyDoctors component to fetch and display doctors list dynamically

--- app/admin/verify-doctors/page.tsx
+++ app/admin/verify-doctors/page.tsx
@@ -1,33 +1,32 @@
 import Table from '@/app/_components/tables';
 import { Button } from '@/components/ui/button';
+import { getDoctorsList } from '@/lib/db-queries';
 import { FileIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
 
-function VerifyDoctors() {
+async function VerifyDoctors() {
+  const { data } = await getDoctorsList();
   return (
     <div className="flex flex-col gap-4">
       <h1>
-        <span className="text-lg font-bold text-muted-foreground">Active/Inactive Doctors List</span>
+        <span className="text-lg font-bold text-muted-foreground">
+          Active/Inactive Doctors List
+        </span>
       </h1>
       <Table
         caption="List of Doctors"
-        rows={[
-          {
-            name: 'Veerbal Singh',
-            email: 'veerbalsing1@gmail.com',
-            specialization: 'Dentist',
-            qualifications: 'BDS',
-            actions: (
-              <div className="flex gap-1 justify-end">
-                <Link href="/admin/doctor/details">
-                  <Button size="sm" variant="default">
-                    <FileIcon />
-                  </Button>
-                </Link>
-              </div>
-            ),
-          },
-        ]}
+        rows={data?.map((doctor) => ({
+          ...doctor,
+          actions: (
+            <div className="flex gap-1 justify-end">
+              <Link href={`/admin/doctor/${doctor.id}`}>
+                <Button size="sm" variant="default">
+                  <FileIcon />
+                </Button>
+              </Link>
+            </div>
+          ),
+        }))}
         columns={[
           {
             header: 'name',
@@ -38,12 +37,8 @@ function VerifyDoctors() {
             value: 'Email',
           },
           {
-            header: 'specialization',
-            value: 'Specialization',
-          },
-          {
-            header: 'qualifications',
-            value: 'Qualifications',
+            header: 'location',
+            value: 'Location',
           },
           {
             className: 'text-right',
