94f8d1bf53d6a3a9e3c70d7bf3b3c04df11c630f
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-10 09:36:06 +0530

    Refactor admin dashboard page layout

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 function AdminPage() {
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-      <p>This is some dummy content related to doctors.</p>
-      <ul>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
+      <p className="mb-2">This is some dummy content related to doctors.</p>
+      <ul className="list-disc pl-4">
         <li>Doctor 1</li>
         <li>Doctor 2</li>
         <li>Doctor 3</li>
