c75dda6a5da8e5085f4587dcc9218b15850c9651
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-12-10 12:20:50 +0530

    Refactor Header component to conditionally render AvatarUI

--- app/_components/layout/header.tsx
+++ app/_components/layout/header.tsx
@@ -1,14 +1,14 @@
 import AvatarUI from '../avatar';
 import Logo from '../logo';
 
-function Header({ menu }: { menu: any }) {
+function Header({ menu, noAvatar }: { menu?: any; noAvatar?: boolean }) {
   return (
     <div className="px-4 py-2 flex justify-between items-center top-0 w-full bg-white sticky z-50 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="flex gap-2">
         <div className="md:hidden">{menu}</div>
         <Logo />
       </div>
-      <AvatarUI />
+      {!noAvatar && <AvatarUI />}
     </div>
   );
 }
